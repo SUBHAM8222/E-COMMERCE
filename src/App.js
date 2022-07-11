bc15037e616a36c7f5096a410aa3fc1b6baf289b
SUBHAM8222 <subhamagrwl@gmail.com>
2022-07-11 12:40:55 +0530

    cart added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React,{useState}from 'react';
 import './App.css';
 import Product from './components/products';
+import Header from './components/Header/Header';
+import Cart from './components/Cart/CART';
 
 const App = () => {
+
+    const [cartshown,cartisshown]=useState(false);
+
+    const showcarthandler=()=>{
+
+        cartisshown(true);
+        
+    }
   
  return(
+    <React.Fragment>
+      {cartshown&&<Cart></Cart>}  
+<Header onclick={showcarthandler}></Header>
 
   <ul><Product></Product></ul>
+  </React.Fragment>
     
   
  )
