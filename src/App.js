27a826a461bce4badcc6ee79f2a6c32f2e8b1d07
unknown <subhamagrwl@gmail.com>
2022-07-18 12:42:55 +0530

    products details using param

--- src/App.js
+++ src/App.js
@@ -1,60 +1,63 @@
-import React,{useState}from 'react';
-import { Route } from 'react-router-dom';
-import './App.css';
-import Product from './components/products';
-import Header from './components/Header/Header';
-import Cart from './components/Cart/CART';
-import CartProvider from './components/Store/CartProvider';
- import About from './components/Pages/About';
-import Home from './components/Pages/Home';
-import Contactus from './components/Pages/Contactus';
-import Movies from './components/Pages/Movies';
+import React, { useState } from "react";
+import { Route,Switch } from "react-router-dom";
 
+import "./App.css";
+import Product from "./components/products";
+import Header from "./components/Header/Header";
+import Cart from "./components/Cart/CART";
+import CartProvider from "./components/Store/CartProvider";
+import About from "./components/Pages/About";
+import Home from "./components/Pages/Home";
+import Contactus from "./components/Pages/Contactus";
+import Movies from "./components/Pages/Movies";
+import ItemDetails from "./components/Pages/ItemDetails";
+//import AuthPage from "./components/Pages/Authpage";
 const App = () => {
+  const [cartshown, cartisshown] = useState(false);
 
-    const [cartshown,cartisshown]=useState(false);
+  const showcarthandler = () => {
+    cartisshown(true);
+  };
+  const hidecarthandler = () => {
+    cartisshown(false);
+  };
 
-    const showcarthandler=()=>{
+  return (
+    <React.Fragment>
+      <main>
+        <Switch>
+        <Route path="/store/:productId">
+              <ItemDetails />
+            </Route>
+          <Route path="/store" >
+            <CartProvider>
+              {cartshown && <Cart onclick={hidecarthandler}></Cart>}
+              <Header onclick={showcarthandler}></Header>
 
-        cartisshown(true);
+              <ul>
+                <Product></Product>
+              </ul>
+            </CartProvider>
+           
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/contactus">
+            <Contactus></Contactus>
+          </Route>
+
+          <Route path="/movies" exact>
+            <Movies />
+          </Route>
+          <Route path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
         
-    }
-   const  hidecarthandler=()=>{
-      cartisshown(false);
-    }
-  
- return(
-  <React.Fragment>
-  
-      
-   <main>
-    <Route path='/store'>
-    <CartProvider>
-      {cartshown&&<Cart onclick={hidecarthandler}></Cart>}
- <Header onclick={showcarthandler}></Header> 
+      </main>
+    </React.Fragment>
+  );
+};
 
-   <ul><Product></Product></ul> 
-   </CartProvider> 
-
-    </Route>
-   <Route path='/about'>
-    <About></About>
-  </Route>
-  <Route path='/contactus'>
-    <Contactus></Contactus>
-  </Route>
-  <Route path='/Home'>
-    <Home></Home>
-  </Route>
-  <Route path='/movies'>
-    <Movies/>
-  </Route>
-   </main>
-   
-   </React.Fragment>
-//  <Contactus></Contactus>
-    
- )
-}
-
- export default App;
+export default App;
