3b10b5666b644c31a404fc81e79752b326e76b81
unknown <subhamagrwl@gmail.com>
2022-07-20 00:54:19 +0530

    master

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,15 +1,34 @@
+import {Link} from "react-router-dom";
+import axios from 'axios';
 import React,{useContext} from 'react';
 import './Productlist.css';
 import Cartcontext from './Store/Cart-context';
+//import AuthContext from "./Store/Auth-Context";
 const ProductList=(props)=>{
 
  const cartctx=useContext(Cartcontext);
+ //const authcntx=useContext(AuthContext);
 
+const Email=localStorage.getItem('Email');
 
- const  additemstocarthandler=()=>{
+const E_mail=Email.replace('@','').replace('.','');
+ const  additemstocarthandler=async()=>{
     
-    cartctx.additems(props);
-    console.log(props);
+   
+    console.log(E_mail);
+     let cart= cartctx.additems({...props,quantity:1});
+
+     console.log(cart);
+    
+     await  axios.post(`https://crudcrud.com/api/e5dae6a5f1ea443ba4b167509f1aa0a2/cart${E_mail}`,
+      {
+        cart:cart
+       }).then(response=>{
+        console.log(response)
+       }).catch(err=>{
+        console.log(err);
+       })
+      
 
     
    
@@ -22,19 +41,26 @@ const ProductList=(props)=>{
     
     <ul className='products' id={props.id}>
     <div >
+    <Link to={`/store/${props.id}`}>
     <h3> {props.title}</h3>
+        <img  src={props.imageurl} alt='music1'></img></Link>
     <div >
-        <img  src={props.imageurl} alt='music1'></img>
+        
     </div>
     <div>
+        
         <span>$
         <span>{props.price}</span>
         </span>
-        <button className='button' type='button' onClick={additemstocarthandler}>ADD TO CART</button>
     </div>
     </div>
+    
+    <button className='button' type='button' onClick={additemstocarthandler}>ADD TO CART</button>
+   
 
 </ul>
+
+
 </React.Fragment>
 
 
